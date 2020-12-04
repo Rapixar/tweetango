4a0f95aa248c935d5cee77d15ca8bd3b1536b110
rapixar <cudraphael@gmail.com>
2020-12-04 12:55:57 +0100

    Initial commit

---
+++ tweets/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Tweet(models.Model):
+    id = models.AutoField(primary_key=True)
+    content = models.TextField(null=True)
+    image = models.FileField(upload_to='images/', blank=True)
+# Create your models here.
