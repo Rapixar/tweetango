6f54eaa6de3e7ab9fd6f409cac90806bab68fbb4
rapixar <cudraphael@gmail.com>
2020-12-04 14:59:02 +0100

    Adding tweet views

---
+++ tweetme2/todo.md
@@ -0,0 +1,16 @@
+1. Users
+   - Registration
+   - Login
+   - LogOut
+   - Profile
+     Image
+     Text
+     Follow Button
+   - Feed
+     - Users feed
+     - Users + following
+   - Tweets
+     - Creating
+       - Text
+       - Image
+     - Delete
--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.http import JsonResponse, Http404
+from .models import Tweet
+
+
+def home_view(request, *args, **kwargs):
+    return render(request, "pages/home.html", context={}, status=200)
+
+
+def tweet_func(response, tweet_id, *args, **kwargs):
+    data = {
+        "id": tweet_id,
+    }
+
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data["content"] = obj.content
+        status = 200
+        print(obj)
+    except:
+        data["message"] = "Not Found"
+        status = 404
+
+    '''data = {
+        "id": tweet_id,
+        "content": obj.content,
+        # "image_path": obj.image.url ,
+    }'''
+
+    return JsonResponse(data, status=status)
+
 
 # Create your views here.
